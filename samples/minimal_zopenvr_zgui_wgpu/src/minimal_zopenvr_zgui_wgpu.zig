73df5117e033879897c688731339880485490f28
Ronald Chen <pyrogx1133@gmail.com>
2024-03-03 01:25:06 -0800

    added openvr system struct

--- samples/minimal_zopenvr_zgui_wgpu/src/minimal_zopenvr_zgui_wgpu.zig
+++ samples/minimal_zopenvr_zgui_wgpu/src/minimal_zopenvr_zgui_wgpu.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const zopenvr = @import("zopenvr");
 
 pub fn main() !void {
-    _ = zopenvr.init(.overlay) catch |init_error| switch (init_error) {
+    const system = zopenvr.init(.overlay) catch |init_error| switch (init_error) {
         error.Init_HmdNotFoundPresenceFailed => @panic(zopenvr.EVRInitError.Init_HmdNotFoundPresenceFailed.asEnglishDescription()),
         error.Init_HmdNotFound => @panic(zopenvr.EVRInitError.Init_HmdNotFound.asEnglishDescription()),
         else => |err| return err,
@@ -11,4 +11,5 @@ pub fn main() !void {
 
     std.debug.print("isRuntimeInstalled {}\n", .{zopenvr.isRuntimeInstalled()});
     std.debug.print("isHmdPresent {}\n", .{zopenvr.isHmdPresent()});
+    std.debug.print("{s}\n", .{system.getRuntimeVersion()});
 }
